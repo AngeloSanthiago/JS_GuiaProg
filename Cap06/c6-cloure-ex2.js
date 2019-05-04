302d87742c83e53e76b123713859b488dba22647
AngeloSanthiago <angelosanthiago@gmail.com>
2019-05-03 21:59:33 -0300

    correção de caminho relativo

--- Cap06/c6-cloure-ex2.js
+++ Cap06/c6-cloure-ex2.js
@@ -1,6 +1,7 @@
 /* Uma função interna pode se tornar acessível externamente
 armazeando a mesma dentro de uma variável global
 */
+alert("entrei arquivo");
 var variavelGlobal;
 function funcaoExterna() {
     alert("Função Externa");
